d660a7f461f22b38818e22bf1a627c754c153689
Joochan <joochan123123@gmail.com>
2025-03-22 21:07:55 +0900

    feat: 함수 위치 변경.

--- GTLEngine/Resource/Shader/StaticMeshShader.hlsl
+++ GTLEngine/Resource/Shader/StaticMeshShader.hlsl
@@ -1,5 +1,8 @@
 #include "MVPShader.hlsli"
 
+Texture2D g_txColor : register(t0);
+SamplerState g_Sample : register(s0);
+
 // StaticMeshShader
 struct VS_INPUT
 {
@@ -8,6 +11,3 @@ struct VS_INPUT
     float4 c : COLOR; // Input color from vertex buffer
     float2 t : TEXTURE;
 };
-
-Texture2D g_txColor : register(t0);
-SamplerState g_Sample : register(s0);
