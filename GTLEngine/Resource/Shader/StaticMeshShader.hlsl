b3c56295a0d18ea21be51f117961d15c313caced
Joochan <joochan123123@gmail.com>
2025-03-22 17:35:59 +0900

    feat: shader 추가.

---
+++ GTLEngine/Resource/Shader/StaticMeshShader.hlsl
@@ -0,0 +1,13 @@
+#include "MVPShader.hlsli"
+
+// StaticMeshShader
+struct VS_INPUT
+{
+    float3 p : POSITION; // Input position from vertex buffer
+    float3 n : NORMAL;
+    float4 c : COLOR; // Input color from vertex buffer
+    float2 t : TEXTURE;
+};
+
+Texture2D g_txColor : register(t0);
+SamplerState g_Sample : register(s0);
